ce053fb8416ce0470258a45543913e5c3664a4ca
gowshikan <raworldit@gmail.com>
2017-07-24 10:00:34 +0530

    Update and rename .gitignore to even.c

---
+++ .gitignore/even.c
@@ -0,0 +1,16 @@
+#include <stdio.h>  
+  
+int main() {  
+    int ass; 
+    printf("Even numbers between 1 to 100\n");  
+    
+     
+    ass = 2;
+    while (ass <= 100) {  
+        printf("%d ", ass);
+        
+        ass = ass + 2;  
+    }  
+  
+    return 0;  
+} 
